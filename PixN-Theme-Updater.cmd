4e33c3a5912eac1c0f038d1229bdc4bebe6c92f4
Rapster-UK <rapsters.tech.hangout@gmail.com>
2024-07-30 10:31:05 +0100

    Update PixN-Theme-Updater.cmd

--- PixN-Theme-Updater.cmd
+++ PixN-Theme-Updater.cmd
@@ -1,32 +1,4 @@
 @echo off
-setlocal
-
-:VBSDynamicBuild
-SET TempVBSFile=%temp%\~tmpSendKeysTemp.vbs
-IF EXIST "%TempVBSFile%" DEL /F /Q "%TempVBSFile%"
-ECHO Set WshShell = WScript.CreateObject("WScript.Shell") >>"%TempVBSFile%"
-ECHO Wscript.Sleep 900                                    >>"%TempVBSFile%"
-ECHO WshShell.SendKeys "{F11}"                            >>"%TempVBSFile%
-ECHO Wscript.Sleep 900                                    >>"%TempVBSFile%"
-
-CSCRIPT //nologo "%TempVBSFile%"
-
-rem Text color code for Light Green is A
-set "colorCode=A"
-color %colorCode%
-
-echo .
-echo Updating the script...
-    >nul 2>&1 bitsadmin /transfer update /download /priority foreground "https://contoso.com/script.bat" "%temp%\%~nx0"
-    >nul 2>&1 move /y "%temp%\%~nx0" "%~0"
-    (start "%~dp0" "%~0" updated) & exit /b
-)
-cd /d "%~dp0"
-
-rem Updated code from here onwards.....
-echo NEW 10:22
-pause
-
 rem Read from ASCII.txt and visualize ASCII art
 type ASCII.txt
 
