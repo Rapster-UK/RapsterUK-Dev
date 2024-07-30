1a689215b0c8227bfbed98ebf964eb4d25c4325c
Rapster-UK <rapsters.tech.hangout@gmail.com>
2024-07-30 13:17:14 +0100

    Update PixN-RB-Update-Service-Payload.cmd

--- PixN-RB-Update-Service-Payload.cmd
+++ PixN-RB-Update-Service-Payload.cmd
@@ -20,14 +20,15 @@ type ASCII.txt
 echo .
 echo Pixel Nostalgia updater running...
 echo .
+ping -n 2 127.0.0.1 > nul
 echo Updating Hypermax-Plus-PixN Theme...
 
 cd ..\..\emulationstation\.emulationstation\themes\Hypermax-Plus-PixN
 ..\..\..\..\emulators\pixn\PortableGit\cmd\git pull
-
+ping -n 2 127.0.0.1 > nul
 echo .
 echo Updating Ckau-Book-PixN Theme...
-
+ping -n 2 127.0.0.1 > nul
 cd ..\ckau-book-PixN
 ..\..\..\..\emulators\pixn\PortableGit\cmd\git pull
 
